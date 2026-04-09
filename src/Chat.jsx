84704c7111ced1ef01b55ecbd4efc567e48c90b3
Ярослав <andenkoyaroslav@gmail.com>
2026-04-09 19:20:06 +0300

    Add files via upload

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -47,7 +47,6 @@ export default function Chat() {
           event: "INSERT",
           schema: "public",
           table: "msg",
-          filter: `getter=eq.${user.id}`,
         },
         (payload) => {
           const newMessage = payload.new;
